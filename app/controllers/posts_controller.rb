f4f71ed1474a8a6e3d4eff539e53101cfb4417b7
Jonathan Alexander <jonathandalexander@gmail.com>
2016-07-08 16:34:11 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,8 @@ class PostsController < ApplicationController
   # GET /posts/new
   def new
     @post = Post.new
+    @post.tags.build
+    # build tags
   end
 
   # GET /posts/1/edit
@@ -25,6 +27,10 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    @post.save
+    # @post.tags.create(post_params[:tags_attributes])
+    
+
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
@@ -68,6 +74,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :content, :tag_ids => [])
+      params.require(:post).permit(:name, :content, :tags_attributes => [:name])
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,18 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags, reject_if: proc { |attr| attr['name'].blank? }
+
+  #accepts_nested_attributes_for :tags
 
   validates_presence_of :name, :content
+
+  # def tags_attributes=(tags_hashes)
+  #   #binding.pry
+  #   tags_hashes.values.each do |tag_params|
+  #     binding.pry
+  #     self.tags.build(tag_params) if tag_params[:name].present?
+  #   end
+  # end
+
 end
